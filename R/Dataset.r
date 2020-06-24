a26a593dcd0c29f67292d8343a5ee6366f6f51d6
Gibran Hemani <explodecomputer@gmail.com>
2020-06-24 12:50:14 +0100

    upd

--- R/Dataset.r
+++ R/Dataset.r
@@ -133,15 +133,15 @@ Dataset <- R6::R6Class("Dataset", list(
 		stopifnot(all(is.numeric(out$pos)))
 		stopifnot(all(is.numeric(out$beta)))
 		stopifnot(all(is.numeric(out$pval)))
-		is_all_1 <- all.equal(out$se, rep(1, length(out$se)), tol=0.05)
-		stopifnot(!is_all_1)
-
 		index <- is.na(out$se)
 		if(any(index))
 		{
 			message("Deriving se for ", sum(index), " out of ", length(index), " entries")
 			out$se[index] <- self$se_from_bp(out$beta, out$pval)
 		}
+		is_all_1 <- all.equal(out$se, rep(1, length(out$se)), tol=0.05)
+		stopifnot(!is_all_1)
+
 
 		j <- length(required_columns)
 		for(i in optional_columns)
