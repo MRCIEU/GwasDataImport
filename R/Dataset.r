a57c0a2cd48790b2d3a6aed8a37165d9c65c73d5
Philip Haycock <philip.haycock@bristol.ac.uk>
2023-04-28 13:29:00 +0100

    major updated to metadata checks

--- R/Dataset.r
+++ R/Dataset.r
@@ -17,6 +17,8 @@ Dataset <- R6::R6Class("Dataset", list(
 	nsnp = NULL,
 	#' @field metadata List of meta-data entries
 	metadata = NULL,
+	#' @field metadata_test List of outputs from tests of the effect allele, effect allele frequency columns and summary data using CheckSumStats
+	metadata_test = NULL,
 	#' @field metadata_file Path to meta-data json file
 	metadata_file = NULL,
 	#' @field datainfo List of GWAS file parameters
@@ -205,14 +207,29 @@ Dataset <- R6::R6Class("Dataset", list(
 	#' @param gwas_file GWAS filename
 	#' @param gwas_out Filename to save processed dataset to
 	#' @param params Column specifications (see determine_columns for more info)
+	#' @param metadata_test  List of outputs from tests of the effect allele, effect allele frequency columns and summary data using CheckSumStats
 	#' @param ... Further arguments to pass to data.table::fread in order to correctly read the dataset
 	
-	format_dataset = function(gwas_file=self$filename, gwas_out = file.path(self$wd, "format.txt.gz"), params=self$params, ...)
+	format_dataset = function(gwas_file=self$filename, gwas_out = file.path(self$wd, "format.txt.gz"), params=self$params,metadata_test=self$metadata_test, ...)
 	{
-		out <- self$determine_columns(gwas_file=gwas_file, params=params, nrows=Inf, ...)
-		self$check_meta_data(out=out) 
-		# out<-self$determine_columns(gwas_file=gwas_file, params=params, nrows=Inf)
+		# gwas_file=x$filename
+		# gwas_out = file.path(x$wd, "format.txt.gz")
+		# params=x$params
+		# metadata_test=x$metadata_test
+		
 
+		# Check that CheckSumStats tests passed
+		a<-unlist(metadata_test$eaf_conflicts)
+		if(a[names(a)=="test"] == "fail") stop("The CheckSumStats test of the effect allele frequency column failed")
+		a<-unlist(metadata_test$gc_conflicts)
+		if(a[names(a)=="test"] == "strong_fail") stop("The CheckSumStats of the effect allele column failed")
+		if(a[names(a)=="test"] == "moderate_fail") warning("The CheckSumStats of the effect allele column partially failed")
+		a<-unlist(metadata_test$false_positive_hits)
+		if(a[names(a)=="test"] == "fail") warning("The CheckSumStats test of reported GWAS hits failed. The GWAS hits in the test dataset are not present in the GWAS catalog")		
+
+
+		out <- self$determine_columns(gwas_file=gwas_file, params=params, nrows=Inf, ...)
+		
 		self$datainfo[["gwas_file"]] <- gwas_out
 		message("Determining build")
 		out <- liftover_gwas(out, chr_col=self$datainfo$chr_col, pos_col=self$datainfo$pos_col, snp_col=self$datainfo$snp_col, ea_col=self$datainfo$ea_col, oa_col=self$datainfo$oa_col)
@@ -309,33 +326,63 @@ Dataset <- R6::R6Class("Dataset", list(
 	},
 
 	#' @description
-	#' Check effect allele and frequency columns are correct. 
-	#' @param Dataset of GWAS results to check 
-	#' @param trait the GWAS trait 
-	#' @param efo_id identifier for GWAS trait in the experimental factor ontology 
+	#' Check that the reported effect allele and effect allele frequency columns are correct. 
+	#' @param gwas_file Filename to read
+	#' @param params column names from x$determine_columns(). Required columns are: c("snp_col", "ea_col", "oa_col", "eaf_col" ) 
+	#' @param metadata metadata from x$collect_metadata()
 
-	check_meta_data = function(out=NULL,trait=metadata$trait,efo_id=metadata$ontology)
+	check_meta_data = function(gwas_file=self$filename,params=self$params,metadata=self$metadata)
 	{
 		library(CheckSumStats)
-		# out<-data.table::fread(self$gwas_out,nrows=Inf)	
+		out<-data.table::fread(self$filename,nrows=Inf)	
+		# out<-data.table::fread(x$filename,nrows=Inf)	
 		
-		#CheckSumStats expects column names:
-		names(out)[names(out) == "snp"]<-"rsid"
-		names(out)[names(out) == "ea"]<-"effect_allele"
-		names(out)[names(out) == "oa"]<-"other_allele"
+		
+		#CheckSumStats expects these column names:
+		names(out)[names(out) == params$snp_col]<-"rsid"
+		names(out)[names(out) == params$ea_col]<-"effect_allele"
+		names(out)[names(out) == params$oa_col]<-"other_allele"
+		names(out)[names(out) == params$eaf_col]<-"eaf"
+		names(out)[names(out) == params$beta_col]<-"beta"
+		names(out)[names(out) == params$se_col]<-"se"
+		names(out)[names(out) == params$pval_col]<-"p"
+		
+		out$population<-self$metadata$population #checksumstats expects an ancestry column 
+		# out$population<-x$metadata$population
 	
 		# allele frequency conflicts with 1000 genomes super populations implying incorrect effect allele frequency column
-		af_conclicts_function(out=out)
+		af_tests<-NULL
+		af_tests<-af_conflicts_function(out=out)
+
+
+		self$metadata_test$eaf_conflicts <- af_tests[1]
+		self$metadata_test$eaf_conflicts_plot <- af_tests[2]
+
+		# x$metadata_test$eaf_conflicts <- af_tests[1]
+		# x$metadata_test$eaf_conflicts_plot <- af_tests[2]
 
 		
 		# effect size conflicts with GWAS catalog implying incorrect effect allele column
-		# this function can be slow if: 1) there are lots of "GWAS hits" for the trait of interest, 2) when searching on both EFO and trait name and 3) when mapping genetic associations to study_id in the GWAS catalog (this latter step is needed for matching of associations on ancestry). To speed things up, the default is to only search on trait name and to ignore study ancestry. 
+		# this function can be slow if: 1) there are lots of "GWAS hits" for the trait of interest, 2) when searching on both EFO and trait name and 3) when mapping genetic associations to study_id in the GWAS catalog (this latter step is needed for matching of associations on ancestry). To speed things up, the default is to only search on trait name and to ignore study ancestry. If not associations are found on trait name, the search is then done on the user provided ontology. It is assumed that ontology is a required metadata field
 		
-		gc_conflicts_function(out=out,efo_id=NULL,trait=trait,efo_id=NULL,ignore_conflict=FALSE,map_association_to_study=FALSE)	
+		gc_tests<-NULL
+		gc_tests<-gc_conflicts_function(out=out,metadata=metadata)	
+		# efo_id=NULL,trait=trait,efo_id=NULL,ignore_conflict=FALSE,map_association_to_study=FALSE		
+		self$metadata_test$gc_conflicts <- gc_tests[1]
+		self$metadata_test$gc_conflicts_plot <- gc_tests[2]
 
-	},
-	
-	
+		# find reported GWAS hits in the gWAS catalog. absence suggests problems with the provided summary data. e.g. failure to exclude false positives / unreliable associations / to pass GWAS results throigh post GWAS QC
+		false_positive_tests<-NULL
+		# x$metadata_test$eaf_conflicts
+		false_positive_tests<-reported_gwas_hits_in_gwascatalog(out=out,eaf_test=af_tests[1],metadata=metadata)
+		# false_positive_tests<-reported_gwas_hits_in_gwascatalog(out=out,eaf_test=x$metadata_test$eaf_conflicts,metadata=metadata)
+		self$metadata_test$false_positive_hits<-false_positive_tests
+
+
+		},
+
+		
+		
 
 	#' @description
 	#' Write meta data to json file
@@ -356,9 +403,20 @@ Dataset <- R6::R6Class("Dataset", list(
 	#' @description
 	#' Upload meta data to API
 	#' @param metadata List of meta data fields and their values
+	#' @param metadata_test  List of outputs from tests of the effect allele, effect allele frequency columns and summary data using CheckSumStats
 	#' @param access_token Google OAuth2.0 token. See ieugwasr documentation for more info
-	api_metadata_upload = function(metadata=self$metadata, access_token=ieugwasr::check_access_token())
+	api_metadata_upload = function(metadata=self$metadata, metadata_test =self$metadata_test, access_token=ieugwasr::check_access_token())
 	{
+
+		# check that CheckSumStats tests passed
+		a<-unlist(self$metadata_test$eaf_conflicts)
+		if(a[names(a)=="test"] == "fail") stop("effect allele frequency column incorrectly specified")
+		a<-unlist(self$metadata_test$gc_conflicts)
+		if(a[names(a)=="test"] == "strong_fail") stop("effect allele column incorrectly specified")
+		if(a[names(a)=="test"] == "moderate_fail") warning("effect allele column may be incorrectly specified")
+		a<-unlist(self$metadata_test$false_positive_hits)
+		if(a[names(a)=="test"] == "fail") warning("The CheckSumStats test of reported GWAS hits failed. The GWAS hits in the test dataset are not present in the GWAS catalog")	
+
 		o <- ieugwasr::api_query("edit/add", query=metadata, access_token=access_token, method="POST")
 		if(httr::status_code(o) == 200)
 		{
@@ -426,9 +484,19 @@ Dataset <- R6::R6Class("Dataset", list(
 	#' Upload gwas dataset
 	#' @param datainfo List of data column parameters
 	#' @param gwasfile Path to processed gwasfile
+	#' @param metadata_test  List of outputs from tests of the effect allele, effect allele frequency columns and summary data using CheckSumStats
 	#' @param access_token Google OAuth2.0 token. See ieugwasr documentation for more info
-	api_gwasdata_upload = function(datainfo=self$datainfo, gwasfile=self$gwas_out, access_token=ieugwasr::check_access_token())
+	api_gwasdata_upload = function(datainfo=self$datainfo, gwasfile=self$gwas_out,metadata_test=self$metadata_test, access_token=ieugwasr::check_access_token())
 	{
+		# check that CheckSumStats tests passed
+		a<-unlist(self$metadata_test$eaf_conflicts)
+		if(a[names(a)=="test"] == "fail") stop("effect allele frequency column incorrectly specified")
+		a<-unlist(self$metadata_test$gc_conflicts)
+		if(a[names(a)=="test"] == "strong_fail") stop("effect allele column incorrectly specified")
+		if(a[names(a)=="test"] == "moderate_fail") warning("effect allele column may be incorrectly specified")
+		a<-unlist(self$metadata_test$false_positive_hits)
+		if(a[names(a)=="test"] == "fail") warning("The CheckSumStats test of reported GWAS hits failed. The GWAS hits in the test dataset are not present in the GWAS catalog")	
+
 		stopifnot(!is.null(self$igd_id))
 		stopifnot(self$metadata_uploaded)
 		y <- datainfo
@@ -504,33 +572,111 @@ Dataset <- R6::R6Class("Dataset", list(
 	}
 ))
 
-af_conclicts_function<-function(out=NULL,metadata=NULL)
+af_conflicts_function<-function(out=NULL)
 {
-	af_conclicts<-CheckSumStats::flag_af_conflicts(target_dat=out)
-	if(af_conclicts$number_of_snps>10 & af_conclicts$proportion_conflicts>=0.10) stop("allele frequency conflicts identified. It looks like you have incorrectly specified the effect allele frequency column.")
+	af_conflicts<-CheckSumStats::flag_af_conflicts(target_dat=out)
+	af_conflicts_test<-"test not possible"
+	if(af_conflicts$number_of_snps>10 & af_conflicts$proportion_conflicts>=0.10){
+		af_conflicts_test<-"fail"
+	}		
+	if(af_conflicts$number_of_snps>10 & af_conflicts$proportion_conflicts<0.10){
+		af_conflicts_test<-"pass"
+	}		
+	# if(af_conflicts_test=="fail") warning("allele frequency conflicts identified. It looks like you have incorrectly specified the effect allele frequency column.")
+	af_conflicts<-c(af_conflicts,"test"=af_conflicts_test)
+	Plot1<-CheckSumStats::make_plot_maf(ref_1000G=c("AFR","AMR","EAS","EUR","SAS","ALL"),target_dat=out)
+	# af_conflicts<-c(af_conflicts,"plot"=Plot1)
+	return(list(af_conflicts,Plot1))
 }
 
+# stop("allele frequency conflicts identified. It looks like you have incorrectly specified the effect allele frequency column.")
+# , efo_id=NULL,trait=NULL,ignore_conflict=FALSE,map_association_to_study=FALSE
+gc_conflicts_function<-function(out=NULL,metadata=NULL)
+{
 
-gc_conflicts_function<-function(out=NULL,efo_id=NULL,trait=NULL,ignore_conflict=FALSE,map_association_to_study=FALSE){
-
-	# EFO<-CheckSumStats::get_efo(trait=metadata$trait)
-	# library(CheckSumStats)
-	
-	gc_dat<-CheckSumStats::compare_effect_to_gwascatalog2(dat=out,efo_id=efo_id,trait=trait,map_association_to_study=map_association_to_study)
+	gwas_catalog<-CheckSumStats::gwas_catalog_hits(trait=metadata$trait)
+	if(length(gwas_catalog) == 1)
+	{
+		if(gwas_catalog == "no results found")
+		{
+			gwas_catalog<-CheckSumStats::gwas_catalog_hits(efo_id=metadata$ontology)
+		}
+	}
+	gc_dat<-CheckSumStats::compare_effect_to_gwascatalog2(dat=out,gwas_catalog=gwas_catalog,trait=metadata$trait)
+	# if(gc_dat == "no results found"){
+	# 	gc_dat<-CheckSumStats::compare_effect_to_gwascatalog2(dat=out,efo_id=metadata$ontology)
+	# }
 
 	gc_conflicts<-CheckSumStats::flag_gc_conflicts2(gc_dat=gc_dat)	
 	
 	N_snps<-gc_conflicts$effect_size_conflicts$n_snps
-	if(N_snps>10){ 
-		n_high<-unlist(gc_conflicts$effect_size_conflicts["high conflict"])
-		n_moderate<-unlist(gc_conflicts$effect_size_conflicts["moderate conflict"])
-		N_conflicts<-n_high + n_moderate
-		proportion_conflicts<-N_conflicts/N_snps
-		if(ignore_conflict){
-			if(proportion_conflicts>0.4) warning("effect size conflicts with GWAS catalog identified. It looks like you may have incorrectly specified the effect allele column")
-			return(proportion_conflicts)
-		}else{
-			if(proportion_conflicts>0.4) stop("effect size conflicts with GWAS catalog identified. It looks like you may have incorrectly specified the effect allele column.")
+	
+	n_high<-unlist(gc_conflicts$effect_size_conflicts["high conflict"])
+	n_moderate<-unlist(gc_conflicts$effect_size_conflicts["moderate conflict"])
+	N_conflicts<-n_high + n_moderate
+	proportion_conflicts<-N_conflicts/N_snps
+
+	gc_conflicts_test<-"Number of SNPs may be too low for reliable test. Interpret with caution"
+	if(N_snps>10){
+		if(proportion_conflicts >=0.8){ 
+			gc_conflicts_test<-"strong_fail"
+			# warning("Very strong effect size conflicts with GWAS catalog identified. The effect allele column looks wrong")
+		}
+		if(proportion_conflicts<0.8 & proportion_conflicts>0.3){
+			gc_conflicts_test<-"moderate_fail"
+			# warning("Effect size conflicts with GWAS catalog identified. It looks like you may have incorrectly specified the effect allele column")
+		}
+		if(proportion_conflicts<0.3){
+			gc_conflicts_test<-"pass"
 		}
 	}
+	gc_conflicts<-c(gc_conflicts,"test"=gc_conflicts_test)
+	Plot2<-CheckSumStats::make_plot_gwas_catalog(dat=out,gwas_catalog=gwas_catalog,gc_dat=gc_dat)
+	return(list(gc_conflicts,Plot2))
+}
+
+reported_gwas_hits_in_gwascatalog<-function(out=NULL,clump=TRUE,eaf_test=NULL,metadata=NULL)
+{	
+	Pop<-infer_ancestry(target_dat=out)
+	l2<-unlist(Pop)
+	Pop<-names(l2[which(l2==max(l2))])
+	dat<-out[which(out$p<5e-8),]
+	# l<-unlist(eaf_test)
+	# if(l[names(l)=="test"] =="fail") warning("eaf test failed, which might undermine the infer_ancestry test for ld_clumping")
+	if(nrow(dat)==0) return("no GWAS hits at 5e-8 threshold")
+
+	Clump<-ieugwasr::ld_clump(dat=dplyr::tibble(rsid=dat$rsid, pval=dat$p),clump_r2 = 0.001,clump_p=5e-8,pop=Pop) #it seems like there is a bug such that when none of the variants are present in the reference panel this function returns a Server code:503.  
+	gwas_hits<-Clump$rsid
+	if(length(Clump$rsid)<5)
+	{
+		gwas_hits<-dat$rsid
+	}
+
+	gc_list<-find_hits_in_gwas_catalog(gwas_hits=gwas_hits,trait=metadata$trait,distance_threshold=50000) 
+	#if no results are found on reported trait, then use the user provided ontology to find associations
+	if(gc_list == "no results found")
+	{
+		metadata$ontology<-gsub(":","_",metadata$ontology) #currently there is a bug in the gwasrapidd code that does not allow EFOs with semicolons. The semicolon must be replaced with an underscore. I have writtent to the developers to see if this can be fixed.  
+		gc_list<-find_hits_in_gwas_catalog(gwas_hits=gwas_hits,efo_id=metadata$ontology,distance_threshold=50000) 
+	}
+
+	if(gc_list == "no results found") return(gc_list)
+
+	# metadata$ontology
+
+	N_in_gc<-length(gc_list$in_gc)
+	N_not_in_gc<-length(gc_list$not_in_gc )
+	N<-N_not_in_gc+N_in_gc
+	test_hits_gc<-"test not possible"
+	if(N_not_in_gc/N>0.3)
+	{
+		test_hits_gc<-fail
+	}
+	if(N_not_in_gc/N<0.3)
+	{
+		test_hits_gc<-"pass"
+	}
+	false_positive_hits<-c(gc_list,"test"=test_hits_gc)
+
+	return(false_positive_hits)
 }
